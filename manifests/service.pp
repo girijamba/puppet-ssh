8aef480ecaf2157a2bc8598efc97b307eafad6bd
root <root@girijambamaster.puppet.com>
2017-10-11 15:29:40 +0530

    With Params Manifest

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,10 @@
-class ssh::service {
-  service { 'sshd':
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+){
+  service { 'ssh-service':
     ensure 	=> running,
     enable 	=> true,
-    hasstatus 	=> true,
-    hasrestart 	=> true,
+    name	=> $service_name,
 
   }
 }
